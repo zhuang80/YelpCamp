e5af15ff533a5b7cc4b944cb2cb2d2b68de1b3ce
EC2 Default User <ec2-user@ip-172-31-17-112.us-west-2.compute.internal>
2020-05-08 03:28:14 +0000

    add Comment feature

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express"),
       bodyParser = require("body-parser"),
       mongoose = require("mongoose"),
+      Campground = require("./models/campground.js"),
+      seedDB = require("./seeds.js"),
       app = express();
       
 const imageUrl = "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
@@ -8,16 +10,10 @@ const imageUrl = "https://images.unsplash.com/photo-1537565266759-34bbc16be345?i
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
 
+
 mongoose.connect("mongodb://localhost:27017/yelp_camp");
 
-
-const campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-const Campground = mongoose.model("Campground", campgroundSchema);
+seedDB();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -32,7 +28,7 @@ app.get("/campgrounds", (req, res) => {
           console.log("ERROR");
           console.log(err);
       }else{
-          res.render("index", {campgrounds: campgrounds})
+          res.render("campgrounds/index", {campgrounds: campgrounds})
       }
    });
 });
@@ -52,25 +48,38 @@ app.post("/campgrounds", (req, res) => {
 });
 
 app.get("/campgrounds/new", (req, res) => {
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 
 // SHOW - shows more info about one campground 
 app.get("/campgrounds/:id", (req, res) => {
     //find the campground with provided ID
-    console.log("enter the show handler");
-    Campground.findById(req.params.id, (err, foundCamp) => {
+    
+    Campground.findById(req.params.id).populate("comments").exec((err, foundCamp) => {
         if(err) {
             console.log("ERROR");
             console.log(err);
         }else {
             console.log(foundCamp);
-            res.render("show", {campground: foundCamp});
+            res.render("campgrounds/show", {campground: foundCamp});
         }
     })
 });
 
+//==============
+// COMMENTS ROUTES
+//==============
+
+app.get("/campgrounds/:id/comments/new", (req, res) => {
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err) {
+            console.log(err);
+        }else {
+            res.render("comments/new", {campground: campground});
+        }
+    })
+});
 app.listen(process.env.PORT, () => {
     console.log(`The server is running on port ${process.env.PORT}.`);
 });
